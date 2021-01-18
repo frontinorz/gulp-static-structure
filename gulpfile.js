1bc4f26fe5bb11cddb4864444e453c0b39a3d854
frontinorz <frontinorz@gmail.com>
2021-01-18 16:57:50 +0800

    Add purgeCSS, babel, file include

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,30 @@
 const { src, dest, watch, parallel, series } = require("gulp");
 
+//* html 
+const fileinclude = require('gulp-file-include');
+
+//* sass
 const sass = require('gulp-sass')
 const postcss = require('gulp-postcss');
 const cssnano = require('cssnano');
 const autoprefixer = require('autoprefixer')
+const purgecss = require('gulp-purgecss')
+
+//* js
+const babel = require('gulp-babel');
 
 const sync = require("browser-sync").create();
+
+function htmlCompiler(cb) {
+  src('src/*.html')
+    .pipe(fileinclude({
+      prefix: '@@',
+      basepath: '@file'
+    }))
+    .pipe(dest('dist'))
+  cb()
+}
+exports.html = htmlCompiler
 
 function sassCompiler(cb) {
   var plugins = [
@@ -15,6 +34,9 @@ function sassCompiler(cb) {
 
   src('src/scss/*.scss')
     .pipe(sass().on('error', sass.logError))
+    .pipe(purgecss({
+      content: ['src/**/*.html']
+    }))
     .pipe(postcss(plugins))
     .pipe(dest('dist/style'))
     .pipe(sync.stream())
@@ -22,12 +44,13 @@ function sassCompiler(cb) {
 }
 exports.sass = sassCompiler
 
-function htmlCompiler(cb) {
-  src('src/*.html')
-    .pipe(dest('dist'))
-  cb()
+function jsCompiler(cb) {
+  src('src/js/*.js')
+    .pipe(babel())
+    .pipe(dest('dist/js'))
+  cb();
 }
-exports.html = htmlCompiler
+exports.js = jsCompiler
 
 function browserSync(cb) {
   sync.init({
@@ -41,6 +64,5 @@ function browserSync(cb) {
   watch('./dist/*.html').on('change', sync.reload)
 }
 exports.sync = browserSync
-
 
 exports.dev = series(parallel(htmlCompiler, sassCompiler), browserSync)
